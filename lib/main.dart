805b5106651425bb8c217fc30e4e8883454be9a2
TabsOverSpaces4 <guptaharsh.2401@gmail.com>
2021-05-01 22:52:41 +0530

    Secure database

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hmsepm/pages/LSOptions.dart';
 import 'package:hmsepm/pages/book.dart';
@@ -8,9 +9,12 @@ import 'package:hmsepm/pages/Signin.dart';
 import 'package:hmsepm/pages/form.dart';
 import 'package:hmsepm/pages/bill.dart';
 
-
-
-void main() => runApp(MaterialApp(initialRoute: '/login', routes: {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    initialRoute: '/login',
+    routes: {
       '/': (context) => Loading(),
       '/login': (context) => Login(),
       '/gallery': (context) => Gallery(),
@@ -19,6 +23,6 @@ void main() => runApp(MaterialApp(initialRoute: '/login', routes: {
       '/signin': (context) => SignIn(),
       '/form': (context) => FormScreen(),
       '/bill': (context) => Bill(),
-     
     },
-    ));
+  ));
+}
