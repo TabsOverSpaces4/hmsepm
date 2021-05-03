d6d7220f87b4242558fd5c9be6b233ed724dbf59
TabsOverSpaces4 <guptaharsh.2401@gmail.com>
2021-05-03 18:12:45 +0530

    Api and set

---
+++ lib/pages/authentication.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Authentication extends StatefulWidget {
+  @override
+  _AuthenticationState createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(),
+    );
+  }
+}
