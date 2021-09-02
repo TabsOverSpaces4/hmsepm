ebd4b880c5fd66acc810bbc7ee653f98d8c6cdee
TabsOverSpaces4 <guptaharsh.2401@gmail.com>
2021-09-02 20:15:32 +0530

    Distance control testing

--- lib/tabs/home.dart
+++ lib/tabs/home.dart
@@ -147,7 +147,7 @@ class _TravelAppState extends State<HomeTab> {
                                   travelCard(
                                       urls[6], "Visit Kodai", "37Kms", 4),
                                   travelCard(urls[8],
-                                      "bannerghatta national park", "12kms", 4),
+                                      "bannerghatta national park", "18kms", 4),
                                 ],
                               ),
                             ),
